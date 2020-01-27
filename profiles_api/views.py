2d8dd6d3ac94596909b9e281f7197c65e3dc65de
Ganapati Bhat <bhatganapati6@gmail.com>
2020-01-27 23:09:31 +0530

    need to fix django runserver autoreloading

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API view"""
+
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as functions(get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over you application logic',
+            'Is mapped manually to URLS',
+        ]
+
+        return Response({'message':'Hello', 'an_apiview':an_apiview})
+
